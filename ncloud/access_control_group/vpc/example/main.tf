20dc3f071f0a240b25d10968e7f02454ac45c17b
GTRekter <porta.ivan@outlook.com>
2024-04-15 14:10:26 +0900

    Add Access Control Group example

--- ncloud/access_control_group/vpc/example/main.tf
+++ ncloud/access_control_group/vpc/example/main.tf
@@ -1,10 +1,10 @@
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     ncloud = {
       source = "NaverCloudPlatform/ncloud"
     }
   }
-  required_version = ">= 0.13"
 }
 
 provider "ncloud" {}
@@ -14,7 +14,7 @@ resource "ncloud_vpc" "vpc" {
 }
 
 module "access_control_group" {
-  source      = "../../../access_control_group/vpc"
+  source      = "../access_control_group/vpc"
   vpc_no      = ncloud_vpc.vpc.id
   name        = "example-acg"
   description = "example description"
